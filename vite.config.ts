2d6f365499e01110bd7e93a29f13082f1d8df2fe
Saransh Pachhai <saranshpachhai@Macbooks-MacBook-Pro.local>
2024-06-30 00:39:10 +0545

    github pages issues check

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/react-ts-portfolio/",
   plugins: [react()],
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
 });
